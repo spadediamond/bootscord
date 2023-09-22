327102f4ef50c47c6a718217a9dfc4cfafe89cf0
spadediamond <spadediamond1@gmail.com>
2023-09-21 20:40:05 -0400

    initial commit

---
+++ src/main/java/com/kosmos/bootscord/BootscordApplication.java
@@ -0,0 +1,13 @@
+package com.kosmos.bootscord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootscordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosmos/bootscord/BootscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos.bootscord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
