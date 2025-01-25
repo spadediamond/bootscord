f5a6362ce91ce6c308937b0fc3559907d1df0c65
Raoul Nelson <nelson.raoula@gmail.com>
2025-01-25 00:47:13 -0500

    Implement registration based on JSON response -Omitted Thymeleaf for submission for JavaScript processing JSON from API call -Added JSON error response for duplicate username/email -Added styling to username and email taken messages for register form

--- src/main/java/com/kosmos/bootscord/controller/UserController.java
+++ src/main/java/com/kosmos/bootscord/controller/UserController.java
@@ -3,10 +3,13 @@ package com.kosmos.bootscord.controller;
 import com.kosmos.bootscord.entities.BootscordUser;
 import com.kosmos.bootscord.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -19,10 +22,10 @@ public class UserController {
         this.service = service;
     }
 
-    // Find a better way to make login page default..?
+    // Redirects to the homepage (login)
     @GetMapping("/")
-    public RedirectView redirectToLoginForm() {
-        return new RedirectView("/login");
+    public String showHomePage() {
+        return "redirect:/login";
     }
 
     @PostMapping("/createUser")
@@ -35,33 +38,42 @@ public class UserController {
     // Exposes endpoint /login to show login form
     @GetMapping("/login")
     public String showLoginForm() {
-        return "login_page";
+        return "login_form";
     }
 
     // Exposes endpoint /register to show registration form
     @GetMapping("/register")
     public String showRegisterForm(Model model) {
-        // Create a model attribute to bind form data
-        BootscordUser user = new BootscordUser();
-
-        model.addAttribute("user", user);
-        return "register_page";
+        // Create a new model attribute to bind form data
+        model.addAttribute("userForm", new BootscordUser());
+        return "register_form";
     }
 
-    // Process the form and save the Bootscord user
-    @PostMapping("/register/registerUser")
-    public RedirectView registerUser(@ModelAttribute("user") BootscordUser user) {
-        // Save user to database
+    // Check for duplicates, return JSON response and save Bootscord user
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody BootscordUser user) {
+        // Send JSON error if username is taken
+        if (service.isDuplicateUserName(user.getUserName())) {
+            // Return JSON error response with HTTP 409 (Conflict)
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(Map.of("error", "Username is already taken."));
+        }
+        // Send JSON error if email is taken
+        if (service.isDuplicateEmail(user.getEmail())) {
+            // Return JSON error response with HTTP 409 (Conflict)
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(Map.of("error", "Email is already taken."));
+        }
+        // If all checks passed, save user to database and return HTTP 200 (OK)
         service.registerUser(user);
-
-        // Redirect to login page to prevent duplicate submissions
-        return new RedirectView("redirect:/registrationSuccess");
+        return ResponseEntity.ok(Map.of("success", true));
     }
 
-    // Expose endpoint /registrationSuccess to expose registration success page
-    @GetMapping("/registrationSuccess")
-    public String showRegistrationSuccess() {
-        return "registration_success";
+    @GetMapping("/register_result")
+    public String showRegisterResult() {
+        return "register_result";
     }
 }
 
